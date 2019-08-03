1bed8e3684899dfa6b6e1be45628951daf5c3c7b
Christopher <christopher.bielen@nascom.be>
2019-08-03 13:22:43 +0200

    Made boop command case insensitive

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', msg => {
 
     if(msg.author.bot)
         return;
-    if (msg.content.startsWith('boop')) {
+    if (msg.content.toLowerCase().startsWith('boop')) {
         if(msg.mentions !== null && !msg.mentions.everyone && msg.mentions.users.array().length > 0) {
             let users = msg.mentions.users.array();
             for(let i = 0; i < users.length; i++)
